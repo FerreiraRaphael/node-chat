85067956713d7042674b7ec82cf447b6422e978c
raphael ferreira <raphael.ferreira@workcard.com.br>
2017-07-13 15:10:38 -0300

    chat messages render

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    io.emit('connect');
+    console.log('a user connected');
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
